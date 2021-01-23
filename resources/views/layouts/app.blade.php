818ae8111e3286462e46f9050496b7f4b2a7ae76
qkohst <7311626-qkohst@users.noreply.gitlab.com>
2021-01-24 00:18:17 +0700

    Create Controller & Show Index User Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,6 +63,7 @@
       <div id="navbar" class="navbar-collapse collapse">
         <ul class="nav navbar-nav">
           <li class="active"><a href="#">Home</a></li>
+          <li class=""><a href="{{url('/user')}}">User</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
           <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/user', 'UserController');
+
+Route::get('/table/user', 'UserController@dataTable')->name('table.user');
