daf416f80806ba46805302cfbe37bd170205e7ad
qkohst <7311626-qkohst@users.noreply.gitlab.com>
2021-01-24 03:02:52 +0700

    Show Detail Data in HTML Response

---
+++ resources/views/pages/user/show.blade.php
@@ -0,0 +1,12 @@
+<table class="table table-hover">
+  <tr>
+    <th>ID</th>
+    <th>Nama</th>
+    <th>EMail</th>
+  </tr>
+  <tr>
+    <td>{{$model->id}}</td>
+    <td>{{$model->name}}</td>
+    <td>{{$model->email}}</td>
+  </tr>
+</table>
